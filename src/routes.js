06200808b6282b0616ba09639c426a040e2a0f13
RainanAraujo <rainanaraaujo@gmail.com>
2020-08-01 23:03:51 -0300

    test redirect

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Propostas from "./pages/Propostas";
 export default function MainRoutes() {
   return (
     <Switch>
+      <Route path="/propostas" component={Home} />
       <Route path="/" component={Home} />
     </Switch>
   );
